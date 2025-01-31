a994268b781096abff77f46d1287329735cbf798
Ileass Bouhout <ileass.bouhout@gmail.com>
2025-01-31 14:43:58 -0500

    test

--- api/views.py
+++ api/views.py
@@ -13,8 +13,6 @@ def register_view(request):
         form = registerform()
     return render(request, 'api/register.html/', {'form': form})
 
-#blablabla
-
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
