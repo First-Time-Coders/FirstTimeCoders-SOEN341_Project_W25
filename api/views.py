4160a075f5a8a0cb9d55ce8eaee602f0efd378d0
Ileass Bouhout <ileass.bouhout@gmail.com>
2025-02-06 23:14:49 -0500

    logout button in dashboard

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+import bcrypt
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from .forms import registerform
@@ -13,7 +14,7 @@ SUPABASE_URL = "https://rsdvkupcprtchpzuxgtd.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzZHZrdXBjcHJ0Y2hwenV4Z3RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwODEyNDIsImV4cCI6MjA1MzY1NzI0Mn0.9SQn2rXp4j6p8Em_FVhEHukZdzpYqV4lF5T8PT_gVAc"
 
 # Initialize Supabase client
-supabase_client = supabase.create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+supabase_client = supabase.create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def register_view(request):
     if request.method == 'POST':
@@ -61,13 +62,17 @@ def login_view(request):
         # Authenticate with Supabase
         response = supabase_client.auth.sign_in_with_password({"email": email, "password": password})
 
-        if response:
-            request.session['user'] = response.user.id  # Store user session
-            return redirect('dashboard')  # Redirect to dashboard
-        else:
-            return render(request, "api/login.html", {"error": "Invalid credentials."})
+        # Fetch user role from Supabase
+        user_query = supabase_client.table("users").select("role").eq("email", email).single().execute()
+        user_role = user_query.data["role"]
 
-    return render(request, "api/login.html")
+        # Redirect based on role
+        if user_role == "admin":
+                return redirect('dashboard-admin')
+        else:
+                return redirect('dashboard')  # Redirect to dashboard
+    else:
+        return render(request, "api/login.html")
 
 def logout_view(request):
     supabase_client.auth.sign_out()
@@ -78,7 +83,8 @@ def home_view(request):
     return render(request, "api/home.html")
 
 def dashboard_admin_view(request):
-    return render(request, "api/dashboard-admin.html")
+
+    return render(request, "api/dashboard-admin.html", {'user': request.user})
 
 #@login_required
 def dashboard_view(request):
