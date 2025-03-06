6ee2509792c1ba4c65545e5fd48baa8c7cf629e1
Ileass Bouhout <ileass.bouhout@gmail.com>
2025-03-05 21:04:44 -0500

    gnagna

--- api/decorators.py
+++ api/decorators.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from django.shortcuts import redirect
 from django.contrib import messages
+from datetime import datetime
 
 SUPABASE_URL = "https://rsdvkupcprtchpzuxgtd.supabase.co"
 
@@ -8,8 +9,16 @@ def supabase_login_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         print("decorator called")
+        # Check if the user is logged in
         if not request.session.get('user_uuid'):
             messages.error(request, "You must be logged in to access this page.")
             return redirect('login')
-        return view_func(request, *args, **kwargs)
+
+        # Add cache-control headers to prevent caching
+        response = view_func(request, *args, **kwargs)
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        return response
+
     return _wrapped_view
