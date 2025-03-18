38efcf1b725346247f977c8990199c6478aca6bf
Sarvesh Sai <76613800+Hellzonedevil@users.noreply.github.com>
2025-03-18 15:53:48 -0400

    DM is working (latest)

--- api/urls.py
+++ api/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('delete-channel/<uuid:channel_id>/', delete_channel, name='delete-channel'),
     path('delete-message/<uuid:message_id>/', delete_message, name='delete-message'),
     path('dm/list/', dm_list_view, name='dm_list'),
+    path('dm/start/', start_dm_view, name='start_dm'),  # Moved this pattern before the generic one
     path('dm/<str:conversation_id>/', dm_view, name='dm'),
-    path('dm/start/', start_dm_view, name='start_dm'),
-
 ]
