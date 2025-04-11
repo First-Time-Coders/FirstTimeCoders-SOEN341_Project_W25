ae6e619c4172df554f55c33d5821a96cab92532b
Saf1mg <maghnisafouane@gmail.com>
2025-04-11 01:52:43 -0400

    ut

--- api/tests.py
+++ api/tests.py
@@ -27,3 +27,53 @@ class LogoutTestCase(TestCase):
 
         self.assertRedirects(response, reverse('login'))
 
+class AddMemberRequestTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.channel_id = '456e4567-e89b-12d3-a456-426614174999'
+        session = self.client.session
+        session['user_uuid'] = '123e4567-e89b-12d3-a456-426614174000'
+        session['username'] = 'Saf'
+        session['role'] = 'Admin'
+        session.save()
+
+    @patch('api.views.supabase_client')
+    def test_admin_adds_member_request(self, mock_supabase_client):
+        target_username = 'newmember'
+
+        mock_users_table = MagicMock()
+        mock_users_table.select.return_value.eq.return_value.execute.return_value.data = [
+            {'id': 'user-456'}
+        ]
+
+        mock_requests_table = MagicMock()
+        mock_requests_table.insert.return_value.execute.return_value.data = [{'status': 'admin_request'}]
+
+        mock_channel_members = MagicMock()
+        mock_channel_members.select.return_value.eq.return_value.execute.return_value.data = []
+
+        mock_channels_created = MagicMock()
+        mock_channels_created.select.return_value.eq.return_value.execute.return_value.data = []
+
+        mock_all_channels = MagicMock()
+        mock_all_channels.select.return_value.in_.return_value.execute.return_value.data = []
+
+        mock_users_list = MagicMock()
+        mock_users_list.select.return_value.neq.return_value.execute.return_value.data = []
+
+        mock_supabase_client.table.side_effect = [
+            mock_users_table,
+            mock_requests_table,
+            mock_channel_members,
+            mock_channels_created,
+            mock_all_channels,
+            mock_users_list
+        ]
+
+        response = self.client.post(
+            reverse('add-member', args=[self.channel_id]),
+            {'username': target_username}
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('dashboard-admin'))
