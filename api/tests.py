b51b77cee7b4aa3bfd1925fe90f42794a51a885d
Saf1mg <maghnisafouane@gmail.com>
2025-04-10 23:31:57 -0400

    ut

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from unittest.mock import patch, MagicMock
+from django.test import Client, TestCase
+from django.urls import reverse
+from django.contrib import messages
 
-# Create your tests here.
+from unittest.mock import patch
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+
+
+class LogoutTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_logout_view(self):
+        session = self.client.session
+        session['user_uuid'] = 'uuid123'
+        session.save()
+
+        response = self.client.get(reverse('logout'))
+        self.assertNotIn('user_uuid', self.client.session)
+        self.assertRedirects(response, reverse('login'))
